44f085936817f37c0f1f2b55d10f4c00ebe6b61a
ghazalehsamoudi <50411257+ghazalehsamoudi@users.noreply.github.com>
2019-05-25 18:58:21 +0430

    Add files via upload

---
+++ max.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+	int a,b,c,max;
+	cout<<"3 adad vared konid: ";
+	cin>>a>>b>>c;
+	max=a;
+	if(b>max)
+	    max=b;
+    if(c>max)
+        max=c;
+    cout<<"max="<<max;
+	    
+	
+}
